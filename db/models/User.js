bec495d5d7a58e26d868be4447b12cd2bc0df424
David L <lassiterda@gmail.com>
2017-06-08 11:54:57 -0400

    Basic login Auth up and running, no persistance or logout

---
+++ db/models/User.js
@@ -0,0 +1,31 @@
+
+module.exports = (sequelize, DataTypes) => {
+  let User = sequelize.define( "Users", {
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+        }
+      },
+    userType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "user",
+      validate: {
+        isIn: [['user', 'admin']]
+      }
+    }
+  });
+
+  return User;
+}
