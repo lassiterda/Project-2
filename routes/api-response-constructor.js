22d23605f2f9a45b118be623a910df73623d848e
David L <lassiterda@gmail.com>
2017-06-11 17:44:02 -0400

    bug fixes to locations.js and response object costructor

--- routes/api-response-constructor.js
+++ routes/api-response-constructor.js
@@ -2,6 +2,6 @@
 module.exports = function(data, message, err) {
   this.status = data ? "success" : "failure";
   this.message = message;
-  this.data = data.constructor === Array ? data : [ data ];
-  this.err = err.constructor === Array ? err : [ err ];
+  this.data = data ? data.constructor === Array ? data : [ data ] : null;
+  this.err = err ? data.constructor === Array ? err : [ err ] : null;
 }
