f3b08f3132713c61daf0712cf06f36cd78878737
Juan Carlos Gonzalez Molina <Lametsu@Juans-Air.T-mobile.com>
2017-06-12 17:02:34 -0400

    moving forward on home page development

--- public/assets/js/map-logic.js
+++ public/assets/js/map-logic.js
@@ -1,51 +1,41 @@
-function initMap() {
-
-        // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(document.getElementById('map-location'), {
-          center: {lat: 35.22, lng: -80.84},
-          scrollwheel: false,
-          zoom: 14
-        });
-        var infowindow = new google.maps.InfoWIndow();
-        var marker;
-      }
-
-function getLocations() {
-  $.get("/api/locations", function (data) {
-    console.log(response.data);
-    let 
-  }).done(function() {
-    initMap();
-  })
-}
-
-getLocations();
-=======
 //function to ajax and get the markers;
 function getLocations() {
-  $.get("/api/location")
-    .done(function(response) {
-      console.log(response);
-      var locationsArray = response.data;
-        for(i = 0; i < locationsArray.length; i++) {
-          var position = new google.maps.LatLng(locationsArray[i].lat, locationsArray[i].lng);
-          bounds.extend(position);
-          marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            animation: google.maps.Animation.DROP,
-            title: locationsArray[i].name
-          });
-        }
-        marker.addListener('click', toggleBounce);
-        marker.addListener('click', getInfo);
-      })
-      .fail(function(err) { console.log(err) })
-}
-function getInfo() {
+
   $.get("/api/location")
   .done(function(response) {
+
+    console.log(response);
+
+    var locationsArray = response.data;
+
+    for(i = 0; i < locationsArray.length; i++) {
+
+      var position = new google.maps.LatLng(locationsArray[i].lat, locationsArray[i].lng);
+
+      bounds.extend(position);
+
+      marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: locationsArray[i].name
+      });
+    }
+
+    marker.addListener('click', toggleBounce);
+    marker.addListener('click', getInfo);
+
+  })
+  .fail(function(err) { console.log(err) })
+}
+
+function getInfo() {
+
+  $.get("/api/location")
+  .done(function(response) {
+
     var locationData = response.data;
+
     locationData = {
       name: locationData.name,
       desc: locationData.description,
@@ -54,9 +44,12 @@ function getInfo() {
       state: locationData.state,
       zip: locationData.zip 
     }
+
   })
   .fail(function(err) {console.log(err)});
 }
+
+
 // function to init the map, with a callback implemented
 function initMap(cb) {
   // Create a map object and specify the DOM element for display.
@@ -71,24 +64,28 @@ function initMap(cb) {
 initMap(getLocations)
 //Need to create info window dynamically from the location
 var infoWindowContent = [
-  ['<div class="info-content">' + '<h3>Title</h3>' + '<p>Address</p>' + '<p>Description</p>' + '<p>City, State</p>' + '<input type="radio>"']
+['<div class="info-content">' + '<h3>Title</h3>' + '<p>Address</p>' + '<p>Description</p>' + '<p>City, State</p>' + '<input type="radio>"']
 ];
  // Display multiple markers on a map
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
+ var infoWindow = new google.maps.InfoWindow(), marker, i;
  // Allow each marker to have an info window    
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infoWindow.setContent(infoWindowContent[i]);
-                infoWindow.open(map, marker);
-            }
-        })(marker, i));
-function toggleBounce() {
+ google.maps.event.addListener(marker, 'click', (function(marker, i) {
+  return function() {
+    infoWindow.setContent(infoWindowContent[i]);
+    infoWindow.open(map, marker);
+  }
+})(marker, i));
+
+
+ function toggleBounce() {
   if (marker.getAnimation() !== null) {
     marker.setAnimation(null);
   } else {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
 }
+
+
 function drop() {
   for (var i =0; i < markerArray.length; i++) {
     setTimeout(function() {
@@ -98,10 +95,10 @@ function drop() {
 }
 //directions function
   //working on...
-function plotDirections() {
-  google.maps.event.addListener(marker, 'click', function(m) {
-  })
-}
+  function plotDirections() {
+    google.maps.event.addListener(marker, 'click', function(m) {
+    })
+  }
 //Modal stuffs
 $("#add-location").on('click', function() {
   $('#myModal').modal('show');
