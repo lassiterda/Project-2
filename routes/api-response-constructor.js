4a986583c143658e250b9722dad14e2ecc32aeed
David L <lassiterda@gmail.com>
2017-06-12 21:25:46 -0400

    bug fix to response object constructor

--- routes/api-response-constructor.js
+++ routes/api-response-constructor.js
@@ -3,5 +3,5 @@ module.exports = function(data, message, err) {
   this.status = data ? "success" : "failure";
   this.message = message;
   this.data = data ? data.constructor === Array ? data : [ data ] : null;
-  this.err = err ? data.constructor === Array ? err : [ err ] : null;
+  this.err = err ? err.constructor === Array ? err : [ err ] : null;
 }
