dfea80beef00769160de6a9341846a131c12fb67
David L <lassiterda@gmail.com>
2017-06-12 20:18:25 -0400

    create route for Trips functioning, small bug fix to user registration route.

--- routes/trips.js
+++ routes/trips.js
@@ -20,35 +20,33 @@ module.exports = function(app) {
 
 
   //update a single trip
-  app.post("api/trip/:id", function(req,res){
-
-    //
-    db.Trip.update(req.body)
-      .then((dbLocation) => { res.json(dbLocation) })
-      .catch((err) => {res.json(err)})
-
-  });
-
-  // app.post("/api/trip/create", function(req,res) {
+  // app.post("api/trip/:id", function(req,res){
+  //
   //   //
-  //   db.Trip.create(req.body)
-  //     .then(dbTrip => {
+  //   db.Trip.update(req.body)
+  //     .then((dbLocation) => { res.json(dbLocation) })
+  //     .catch((err) => {res.json(err)})
   //
-  //     //format the
-  //     let tripLocArr = req.body.locations.map((ele, idx) => {
-  //       return { sequenceNumber: idx + 1, LocationId: ele, TripId: result.dataValues.id}
-  //     });
-  //
-  //     return db.TripLocation.bulkCreate(tripLocArr)
-  //       .then(() => {
-  //
-  //
-  //
-  //       })
-  //
-  //   })
-  //   .catch(err => {res.json(new ResponeObj(null, "Something went wrong, see error message for details.", err) ) })
-  // }
+  // });
+
+  app.post("/api/trip/create", function(req,res) {
+
+    //create the trip based ont the req.body
+    db.Trip.create(req.body)
+      .then(dbTrip => {
+
+      //format the locations array to pass into the db as TripLocations
+      let tripLocArr = req.body.locations.map((ele, idx) => {
+        return { sequenceNumber: idx + 1, LocationId: ele, TripId: dbTrip.dataValues.id}
+      });
+
+      //create the TripLocation objects for the trip
+      db.TripLocation.bulkCreate(tripLocArr)
+        .then((dbTripLocations) => { res.json(new ResponeObj(dbTrip, "Success, Trip created.")) })
+        .catch(err => {res.json(new ResponeObj(null, "Something went wrong, see error message for details.", err) ) })
+    })
+    .catch(err => {res.json(new ResponeObj(null, "Something went wrong, see error message for details.", err) ) })
+  });
 
 
   app.post("/api/trip/delete/:id", function(req, res){
