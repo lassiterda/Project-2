0edbdaac7a658976fbdecf0b42fa7fca3d32df70
David L <lassiterda@gmail.com>
2017-06-15 10:35:39 -0400

    directions logic implemented for previous structure

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,14 @@
-//When the document is ready, the following code will run
+//When the document is ready, the following code will run\
+
+	var locationsGlobal = [];
+
 $(document).ready(function(){
 
 	// =============== Program Logic ===============
 
 	//we're declaring a global array to hold locations data
-	//so that we may access it from anywhere in the program 
+	//so that we may access it from anywhere in the program
 	//without running into scoping issues
-	var locationsGlobal = [];
 
 	//initializes home page
 	initHome();
@@ -42,27 +44,58 @@ $(document).ready(function(){
 	});//end of click-event
 
 	$('#add-location-btn').on('click', function() {
-		
+
 		//prevents the element's default behavior from triggering
 		event.preventDefault();
 		addLocation();
 	});//end of click-event
 
-	$('#build-trip-btn').on('click', function() {
+	// $('#build-trip-btn').on('click', function() {
+	//
+	// 	//prevents the element's default behavior from triggering
+	// 	event.preventDefault();
+	//
+	// 	snatchSelectedLocations();
+	//
+	//
+	//
+	//
+	// });//end of click-event
+$("#build-trip-btn").click(function(){
 
-		//prevents the element's default behavior from triggering
-		event.preventDefault();
+  //check to see if text says buid trips
 
-		snatchSelectedLocations();
+  var removedIds = [];
 
+  //remove all nonselected elements
+	$(".location-box").each(function(idx, ele) {
+		console.log(!$(ele).hasClass("location-box-active"));
+		if (!$(ele).hasClass("location-box-active")) {
 
+			locationsGlobal =locationsGlobal.filter(function(loc) {
+				return loc.about.id != $(ele).attr("location-id")
+			})
+			$(this).fadeOut("fast", function(){
 
+				$(this).remove();
+			})
+		}
+		else {
+			$(this).off()
+		}
+	})
 
-	});//end of click-event
+  // make remaining elements sortable
+  Sortable.create(document.getElementById("side-bar"));
 
+  clearMarkers();
 
+  //can this be done on initHome(), or would it conflict with base `map`?
+  calcRoute(locationsGlobal, function(response) {
+    console.log(response);
+  })
 
-
+})
 
 	// =============== End of Program Logic ===============
 
@@ -111,12 +144,13 @@ $(document).ready(function(){
 
 		//map
 		map = new google.maps.Map(document.getElementById('map-location'), Charlotte);
-
+	    directionsDisplay = new google.maps.DirectionsRenderer();
+	    directionsService = new google.maps.DirectionsService();
+	    directionsDisplay.setMap(map);
 		renderPins(map, data);
 
 	}//end of initMap
 
-	
 	//returns nothing. used to disable or disable My Trips button
 	function areThereTrips() {
 
@@ -132,11 +166,10 @@ $(document).ready(function(){
 		// 	if(true){
 		// 		$("#my-trips-btn").prop("disabled", false);
 		// 	}
-			
+
 		// })
 
 	}//end of areThereTrips
-
 
 	//retrieves user trips from database
 	function getMyTrips() {
@@ -153,11 +186,10 @@ $(document).ready(function(){
 			//we have yet to write it, but we'll recycle
 			//and tweak the code of the renderSideBarWithLocations function
 			renderSideBarWithTrips(data);
-		
+
 		})
 
 	}//end of showMyTrips
-
 
 	function getLocations() {
 
@@ -173,11 +205,10 @@ $(document).ready(function(){
 			//we have yet to write it, but we'll recycle
 			//and tweak the code of the renderSideBarWithLocations function
 			renderSideBarWithLocations(data);
-		
+
 		})
 
 	}
-
 
 	function renderSideBarWithTrips(trips) {
 
@@ -227,8 +258,17 @@ $(document).ready(function(){
 			$newLocation.box.addClass('location-box');
 			$newLocation.box.attr('location-id', locations.data[i].id);
 
-			$newLocation.box.append(locations.data[i].name + "\n");
-			$newLocation.box.append("Address: " + locations.data[i].address);
+			$newLocation.box.append("<h6>"+ locations.data[i].name + "</h6>");
+			$newLocation.box.append("<p>Address: " + locations.data[i].address + "</p>");
+			$newLocation.box.click(function() {
+				if ($(this).hasClass("location-box-active") ) {
+					$(this).removeClass('location-box-active');
+				}
+				else {
+					$(this).addClass('location-box-active');
+				}
+			})
+
 
 
 			//push individual div to global array
@@ -243,18 +283,18 @@ $(document).ready(function(){
 
 	}
 
-
 	function renderPins(map, locations) {
-
+		Arrmarkers = []
 		for(i = 0; i < locations.data.length; i++) {
 			var position = new google.maps.LatLng(locations.data[i].lat, locations.data[i].lng);
-	                
+
 	                marker = new google.maps.Marker({
 	                	position: position,
 	                	map: map,
 	                	animation: google.maps.Animation.DROP,
 	                	title: locations.data[i].name
 	                });
+									Arrmarkers.push(marker);
         }
 
 	}
@@ -290,7 +330,7 @@ $(document).ready(function(){
 				initHome();
 
 			});
-			
+
 			// alertify.success("Location successfully added!");
 
 		})
@@ -318,37 +358,35 @@ $(document).ready(function(){
 
 	}//end of snatchSelectedLocations
 
-	function renderTrip(selectedLocations) {
+	function calcRoute(arrLocs, cb) {
+	    var origin = arrLocs.shift()
+	    var destination = arrLocs.pop();
 
-		let tripLocations = [];
+	    var request = {
+	        origin: {lat:origin.about.lat, lng: origin.about.lng},
+	        destination: {lat: destination.about.lat, lng: destination.about.lng},
+	        waypoints: arrLocs.map(function(ele){
+	          return { location: {lat: ele.about.lat, lng: ele.about.lng }, stopover: true }
+	        }),
+	        travelMode: google.maps.TravelMode["WALKING"]
+	    }
 
-		var list = document.getElementById('my-ui-list');
-		var $listItem = $('<li />');
-
-		selectedLocations.forEach(function(e){
-			$listItem.text(e.about.name);
-			tripLocations.push($listItem);
-		});
-
-		//coordinates and settings for Charlotte Map
-		const Charlotte = {
-			center: {lat: 35.22, lng: -80.84},
-			scrollwheel: false,
-			zoom: 13
-		}
-
-		//new map we'll use 
-		// map = new google.maps.Map(document.getElementById('map-location'), Charlotte);
-		$('#build-trip-modal').modal('show');
-
-		tripLocations.forEach(function(e) {
-			$('#my-ui-list').append(e);
-		});
-
-
-
+	    directionsService.route(request, function(response, status) {
+	        if(status == "OK") {
+	            directionsDisplay.setDirections(response);
+	            cb(response)
+	    }});
 	}
 
+	function clearMarkers() {
+		setMapOnAll(null);
+	}
+
+	function setMapOnAll(map) {
+		for (var i = 0; i < Arrmarkers.length; i++) {
+			Arrmarkers[i].setMap(map);
+		}
+	}
 	// =============== END OF FUNCTIONS ===============
 
 })//end of document.ready
