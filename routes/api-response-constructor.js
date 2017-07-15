7fa2631f529dc6e7e77de44c8bb4a988a02868a7
David L <lassiterda@gmail.com>
2017-07-14 21:24:02 -0400

    cleaning up unused files

--- routes/api-response-constructor.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = function(data, message, err) {
-  this.status = data ? "success" : "failure";
-  this.message = message;
-  this.data = data ? data.constructor === Array ? data : [ data ] : null;
-  this.err = err ? err.constructor === Array ? err : [ err ] : null;
-}
